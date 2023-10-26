0c26d46d844c3a6a52bf77227852720687dc9e83
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-10-26 13:31:37 -0300

    Una pequeña prueba de Interfaz

---
+++ tarea3/src/Código/MiPanel.java
@@ -0,0 +1,15 @@
+package Código;
+import java.awt.Color;
+import java.awt.*;
+import javax.swing.JPanel;
+
+public class MiPanel extends JPanel{
+    public MiPanel(){
+        this.setBackground(Color.CYAN);
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+        g.setColor(Color.red);
+        g.fillRect(40,40,50,30);
+    }
+}
--- tarea3/src/Código/Ventana.java
+++ tarea3/src/Código/Ventana.java
@@ -5,9 +5,16 @@ import javax.swing.*;
 public class Ventana extends JFrame {
     public Ventana(){
         this.setLayout(new BorderLayout());
+
+        this.add(new JButton("sur"), BorderLayout.SOUTH);
+        this.add(new MiPanel(), BorderLayout.CENTER);
+        this.add(new JButton("norte"), BorderLayout.NORTH);
+        this.add(new JButton("este"), BorderLayout.EAST);
+        this.add(new JButton("oeste"), BorderLayout.WEST);
+
         this.setTitle("Expendedor");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(1000,800);
+        this.setSize(400,300);
         this.setVisible(true);
     }
 }
