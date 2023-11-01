9c2c092ba687191ff382a792cb0d5303e3f74203
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-11-01 15:45:09 -0300

    Implementacion de mouse event

--- tarea3/src/Interfaz/MiPanel.java
+++ tarea3/src/Interfaz/MiPanel.java
@@ -1,15 +1,28 @@
 package Interfaz;
 import java.awt.Color;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 
-public class MiPanel extends JPanel{
+public class MiPanel extends JPanel implements MouseListener {
     public MiPanel(){
+        super();
         this.setBackground(Color.CYAN);
     }
     public void paint(Graphics g){
         super.paint(g);
-        g.setColor(Color.red);
-        g.fillRect(40,40,50,30);
+    }
+    public void mouseClicked(MouseEvent e) {
+    }
+    public void mousePressed(MouseEvent e) {
+        System.out.println("press");
+    }
+    public void mouseReleased(MouseEvent e) {
+    }
+    public void mouseEntered(MouseEvent e) {
+    }
+    public void mouseExited(MouseEvent e) {
+
     }
 }
