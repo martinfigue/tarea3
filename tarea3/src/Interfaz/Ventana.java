abe24d1727f438fc21532c62c22f7fb131ea9c06
idiazbarrientos <146099724+idiazbarrientos@users.noreply.github.com>
2023-11-02 11:55:52 -0300

    Arreglo en ventana para ver los dos paneles

--- tarea3/src/Interfaz/Ventana.java
+++ tarea3/src/Interfaz/Ventana.java
@@ -7,14 +7,37 @@ public class Ventana extends JFrame {
     
     Ventana(){
         super();
-        this.setLayout(new GridLayout(2,3));
-        this.add(new JButton(new ImageIcon("coca.jpeg")));
-        this.add(new JButton(new ImageIcon("sprite.jpg")));
-        this.add(new JButton(new ImageIcon("fanta.jpeg")));
-        this.add(new JButton(new ImageIcon("snickers.jpeg")));
-        this.add(new JButton(new ImageIcon("super 8.png")));
+
+
+        PanelExpendedor pizq = new PanelExpendedor();
+        PanelComprador pder = new PanelComprador();
+        this.add(pizq);
+        this.add(pder);
+
+
+        pizq.setLayout(new GridLayout(2,3));
+        pizq.setBounds(50,20,400,300);
+
+        pder.setLayout(new GridLayout(2,2));
+        pder.setBounds(500,100,300,230);
+
+
+        pizq.add(new JButton(new ImageIcon("coca.jpeg")));
+        pizq.add(new JButton(new ImageIcon("sprite.jpg")));
+        pizq.add(new JButton(new ImageIcon("fanta.jpeg")));
+        pizq.add(new JButton(new ImageIcon("snickers.jpeg")));
+        pizq.add(new JButton(new ImageIcon("super 8.png")));
+
+        pder.add(new JButton("100"));
+        pder.add(new JButton("500"));
+        pder.add(new JButton("1000"));
+        pder.add(new JButton("1500"));
+
+
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(400,300);
+        this.setSize(850,550);
         this.setVisible(true);
+
     }
 }
